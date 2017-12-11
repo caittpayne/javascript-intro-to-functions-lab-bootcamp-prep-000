cc6b24595550ab52175f2659f6d936c98ec30dec
Cait Payne <caitpayne@outlook.com>
2017-12-11 03:54:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase()
+}
+
+var lowerCase = "hello!"
+lowerCase.toLowerCase === lowerCase
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase ("I can't hear you!")
+  
+}
+
+  
